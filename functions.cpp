a082d774a9b75d880c7a2b2eb25ca31c68a1ad44
henrikasz1 <58070388+henrikasz1@users.noreply.github.com>
2020-11-11 19:51:28 +0200

    Implement Merkle tree

---
+++ functions.cpp
@@ -0,0 +1,79 @@
+#include "functions.h"
+#include "hash/functions.h"
+
+//generating users (task 1)
+void generateUsers(std::vector<user> &U)
+{
+  user a;
+  //generate random number which will be the balance of a specific user
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_int_distribution<int> amount(100,1000000);
+
+  for(size_t i = 0; i != U_NUM; ++i)
+  {
+    a.name = "user"+std::to_string(i);
+    a.public_key = combine_hash_function(a.name);
+    a.balance = amount(mt);
+    U.push_back(a);
+  }
+}
+//Generating transactions (task 2)
+void generateTransactions(std::vector<transaction> &T, const std::vector<user> &U)
+{
+  unsigned int count = 0;
+  unsigned int num;
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_int_distribution<int> amount(0,999);
+  transaction a;
+  for (size_t i = 0; i != T_NUM; ++i)
+  {
+    num = amount(mt);
+    a.senderID = U[num].public_key;
+    a.recipientID = U[amount(mt)].public_key;
+    a.id = combine_hash_function(a.senderID+a.recipientID+std::to_string(count));
+    std::uniform_int_distribution<int> money(0, U[num].balance);
+    a.amount = money(mt);
+    T.push_back(a);
+    count++;
+  }
+}
+//-------------------
+void mining (block_chain &BC, std::vector<block> &B)
+{
+  std::string zeros;
+  for (size_t i = 0; i != B.size(); ++i)
+  {
+    std::string hash;
+    //Genesis block
+    if (i == 0)
+    {
+      B[i].prevBlockHash = '0';
+    }
+    else
+    {
+      B[i].prevBlockHash = B[i - 1].merkleRootHash;
+    }
+    char diff_target[B[i].difficultyTarget + 1];
+    for (size_t j = 0; j != B[i].difficultyTarget; ++j)
+    {
+      diff_target[j] = '0';
+    }
+    diff_target[B[i].difficultyTarget] = '\0';
+    zeros = diff_target;
+    do{
+      B[i].nonce++;
+      std::stringstream combine;
+      combine << B[i].nonce << B[i].prevBlockHash << B[i].timeStamp << B[i].merkleRootHash << B[i].version;
+      hash = combine_hash_function(combine.str());
+      B[i].merkleRootHash = hash;
+      //std::cout << hash << std::endl;
+      //hash = combine_hash_function(std::to_string(B[i].nonce + ))
+      //B[i].currentHash = combine_hash_function(std::to_string(B[i].nonce) + B[i].prevBlockHash + std::to_string(B[i].timeStamp) + std::to_string(B[i].version) + std::to_string(B[i].nonce));
+    }while(B[i].merkleRootHash.substr(0, B[i].difficultyTarget) != zeros);
+    BC.newBlock(B[i]);
+
+    std::cout << "Block - > " << i << " Nonce - > " << B[i].nonce << "\n" << "Hash - > " << B[i].merkleRootHash << "\n\n";
+  }
+}
---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "functions.h"
+#include "functions.cpp"
+#include "block.cpp"
+#include "block_chain.cpp"
+
+int main(){
+  std::cout << "Program started \n" << std::endl;
+  std::vector<user> U;
+  std::vector<transaction> T;
+  std::vector<transaction> count;
+  generateUsers(U);
+  std::cout << "Users generated" << std::endl;
+  generateTransactions(T, U);
+  std::cout << "Transactions generated" << std::endl;
+  block_chain BC;
+
+  std::random_device rd;
+  std::mt19937 mt(rd());
+
+  std::vector<block> list;
+  while(T.size() >= 100)
+  {
+    std::vector<transaction> A;
+    for (size_t i = 0; i != 100; ++i)
+    {
+      std::uniform_int_distribution<int> index(0, T.size() - 1);
+      unsigned int a = index(mt);
+      A.push_back(T[a]);
+      count.push_back(T[a]);
+      //instantly erasing so that the transaction could not repeat itself
+      T.erase(T.begin() + a);
+    }
+    block B;
+    B.setTr(A);
+    list.push_back(B);
+  }
+  mining(BC, list);
+  for (std::vector<transaction>::iterator i = count.begin(); i != count.end(); ++i)
+  {
+    for (std::vector<user>::iterator j = U.begin(); j != U.end(); ++j)
+    {
+      if ((*i).senderID == (*j).public_key)
+      {
+        (*j).balance -= (*i).amount;
+      }
+    }
+  }
+  return 0;
+}
